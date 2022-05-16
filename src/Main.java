feb7ba7ce25dee725f3ff2dc292cc0aed4b516af
Oceane <oceane.guinard@outlook.fr>
2022-05-16 14:06:52 +0200

    parser et input file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    static int nbmots;
+    static String[] mots;
+    static int[]vals;
+    public static void main(String[] args) {
+        
+    }
+    public static void parse(Scanner sc) {
+        nbmots=Integer.parseInt(sc.nextLine());
+        mots=new String[nbmots];
+        vals=new int[nbmots];
+        for(int i=0;i<nbmots;i++){
+            String[] tmp=sc.nextLine().split(" ");
+            mots[i]=tmp[0];
+            vals[i]=Integer.parseInt(tmp[1]);
+
+        }
+    }
 }
